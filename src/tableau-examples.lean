19cb9e660cdc9f6fc9f4b2ed975a443bd2af00f0
Malvin Gattinger <malvin@w4eg.de>
2022-06-01 18:59:42 +0200

    work on Example 2 in tableau-examples

--- src/tableau-examples.lean
+++ src/tableau-examples.lean
@@ -109,77 +109,76 @@ end
 -- Example 2
 noncomputable example : closedTableau { r ⋏ ~□p, r ↣ □(p ⋏ q) } :=
 begin
-  let α := { r ⋏ ~□p, r ↣ □(p ⋏ q) },
-  change closedTableau α,
-  dsimp at *, -- gets rid of ↣
   apply closedTableau.loc,
   rotate,
   { -- localTableau α
     -- con
-    apply localTableau.byLocalRule (localRule.con (by {simp} : (r ⋏ ~□p) ∈ α )),
+    apply localTableau.byLocalRule,
+    apply localRule.con,
+    simp,
+    split,
+    refl,
+    refl,
     intros branch branch_def,
-    simp only [finset.union_insert, finset.mem_singleton] at branch_def,
+    rw finset.mem_singleton at branch_def,
+    rw finset.union_insert at branch_def,
     -- nCo
-    apply localTableau.byLocalRule (localRule.nCo (by {finish} : ~(r ⋏ (~(p ⋏ q).box)) ∈ branch)),
-    dsimp only at *,
+    apply localTableau.byLocalRule,
+    apply @localRule.nCo _ r (~□(p ⋏ q)),
+    rw branch_def,
+    simp,
     intros b b_in,
-    rw branch_def at b_in,
     simp only [finset.mem_insert, finset.mem_singleton] at b_in,
-    refine if h1 : b = {r, ~□p, ~r} then _ else if h2 : b = {r, ~□p, ~~□(p⋏q)} then _ else _,
+    -- rw branch_def at b_in,
+    refine if h1 : b = branch \ {~(r⋏~□(p⋏q))} ∪ {~r} then _ else _,
     { rw h1, -- right branch
       -- not:
-      apply localTableau.byLocalRule (localRule.not (by {simp} : r ∈ { r, ~p.box, ~r} ∧ ~r ∈ { r, ~p.box, ~r})),
-      exact emptyTableau,
+      apply localTableau.byLocalRule (localRule.not _) emptyTableau,
+      use r, simp, finish,
     },
-    { rw h2, --left branch
+    { have h2 : b = branch \ {~(r⋏~□(p⋏q))} ∪ {~~□(p⋏q)}, { tauto, },
+      rw h2, --left branch
       -- neg:
-      apply localTableau.byLocalRule (localRule.neg (by {simp} : ~~□(p ⋏ q) ∈ { r, ~□p, ~~□(p ⋏ q) })),
+      apply localTableau.byLocalRule (@localRule.neg _ (□(p ⋏ q)) _),
+      rotate, simp,
       intros child childDef,
-      simp only [finset.mem_singleton] at *,
+      rw finset.mem_singleton at childDef,
       rw childDef,
       -- ending local tableau with a simple node:
-      apply localTableau.sim (by { unfold simple, simp at *, tauto, } : simple {r, ~□p, □(p ⋏ q)} ),
+      rw branch_def,
+      apply localTableau.sim,
+      unfold simple, simp at *, split, unfold simpleForm,
+      intros f f_notDef1 f_notDef2 f_in_branch,
+      cases f_in_branch,
+      rw f_in_branch, unfold r,
+      cases f_in_branch,
+      rw f_in_branch, unfold simpleForm,
+      rw f_in_branch, unfold simpleForm,
+      tauto,
     },
-    { itauto, }
   },
   { -- tableau for the simple end nodes:
-    intro Y,
-    intro Yin,
-    rw setEndNodes at Yin,
-    simp at Yin,
-    -- rw help at Yin,
-    apply classical.choice,
-    rcases Yin with ⟨a,h,YnotRbranch|YnotnotBranch⟩,
+    rw conEndNodes,
+    rw nCoEndNodes,
+    intros Y Yin,
+    rw finset.mem_union at Yin,
+    apply classical.choice, -- NOTE: This should not be needed?
+    cases Yin,
     { -- not-R branch
       exfalso, -- show that there are no endnodes!
-      subst h,
-      sorry,
+      simp at *,
+      exact Yin,
     },
     { -- notnotbranch
       have Yis : Y = {r, ~□p, □(p ⋏ q)}, {
-        subst h,
-        sorry,
+        dsimp at Yin,
+        simp at Yin,
+        sorry, -- How to simplify a 'dite false'?
       },
       subst Yis,
       fconstructor,
       exact subTabForEx2,
     },
-  },
-end
 
--- Problematic "infinite not-progressing tableau"
-def explosion : nonempty (closedTableau { p }) :=
-begin
-  fconstructor,
-  -- this should not be allowed, proof checking takes forever!
-  -- repeat {
-    apply closedTableau.loc,
-    rotate,
-    apply localTableau.sim,
-    { unfold simple, simp, },
-    intros Y Yin,
-    simp at Yin,
-    subst Yin,
-  -- },
-  sorry,
+  },
 end
