cfede915552a59a3f58c43c2c4eb26cfc61624c3
Malvin Gattinger <malvin@w4eg.de>
2022-06-08 17:35:27 +0200

    eliminate last "sorry" in Example 2

--- src/tableau-examples.lean
+++ src/tableau-examples.lean
@@ -79,8 +79,6 @@ begin
   },
 end
 
-
-
 -- preparing example 2
 def subTabForEx2 : closedTableau {r, ~□p, □(p ⋏ q)} :=
 begin
@@ -105,9 +103,8 @@ begin
 },
 end
 
-
 -- Example 2
-noncomputable example : closedTableau { r ⋏ ~□p, r ↣ □(p ⋏ q) } :=
+example : closedTableau { r ⋏ ~□p, r ↣ □(p ⋏ q) } :=
 begin
   apply closedTableau.loc,
   rotate,
@@ -129,56 +126,61 @@ begin
     simp,
     intros b b_in,
     simp only [finset.mem_insert, finset.mem_singleton] at b_in,
-    -- rw branch_def at b_in,
     refine if h1 : b = branch \ {~(r⋏~□(p⋏q))} ∪ {~r} then _ else _,
-    { rw h1, -- right branch
+    { -- right branch
       -- not:
-      apply localTableau.byLocalRule (localRule.not _) emptyTableau,
-      use r, simp, finish,
+      apply localTableau.byLocalRule (@localRule.not _ r _) emptyTableau,
+      finish,
     },
-    { have h2 : b = branch \ {~(r⋏~□(p⋏q))} ∪ {~~□(p⋏q)}, { tauto, },
-      rw h2, --left branch
+    { --left branch
+      have h2 : b = branch \ {~(r⋏~□(p⋏q))} ∪ {~~□(p⋏q)}, { tauto, },
       -- neg:
       apply localTableau.byLocalRule (@localRule.neg _ (□(p ⋏ q)) _),
-      rotate, simp,
+      rotate, {rw h2, simp,},
       intros child childDef,
+      -- ending local tableau with a simple node:
+      apply localTableau.sim,
       rw finset.mem_singleton at childDef,
       rw childDef,
-      -- ending local tableau with a simple node:
-      rw branch_def,
-      apply localTableau.sim,
       unfold simple, simp at *, split, unfold simpleForm,
-      intros f f_notDef1 f_notDef2 f_in_branch,
-      cases f_in_branch,
-      rw f_in_branch, unfold r,
-      cases f_in_branch,
-      rw f_in_branch, unfold simpleForm,
-      rw f_in_branch, unfold simpleForm,
+      intros f f_notDef1 f_in_branch,
+      cases b_in,
       tauto,
+      rw b_in at f_in_branch,
+      simp at f_in_branch,
+      cases f_in_branch,
+      tauto,
+      rw branch_def at f_in_branch,
+      cases f_in_branch with l r, simp at r,
+      cases r,
+      { subst r, unfold r, },
+      cases r,
+      { subst r, unfold simpleForm, },
+      { exfalso, tauto, },
     },
   },
   { -- tableau for the simple end nodes:
     rw conEndNodes,
     rw nCoEndNodes,
     intros Y Yin,
-    rw finset.mem_union at Yin,
-    apply classical.choice, -- NOTE: This should not be needed?
-    cases Yin,
+    simp at *,
+    split_ifs at *,
     { -- not-R branch
       exfalso, -- show that there are no endnodes!
-      simp at *,
+      simp at Yin,
       exact Yin,
     },
     { -- notnotbranch
+      dedup,
       have Yis : Y = {r, ~□p, □(p ⋏ q)}, {
-        dsimp at Yin,
-        simp at Yin,
-        sorry, -- How to simplify a 'dite false'?
+        simp at *,
+        subst Yin,
+        simp at *,
+        ext1,
+        split ; { intro hyp, finish, },
       },
       subst Yis,
-      fconstructor,
       exact subTabForEx2,
     },
-
   },
 end
